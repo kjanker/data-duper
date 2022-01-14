2e20aaed809da4160569f02ed573cc3a28086b4b
kjanker <34544392+kjanker@users.noreply.github.com>
2022-01-14 14:08:49 +0100

    docstrings and improved typing

--- duper/generator/base.py
+++ duper/generator/base.py
@@ -1,15 +1,22 @@
 """
 Module containing the abstract base class for all generators.
 """
+from __future__ import annotations
+
+from typing import List
+
 import numpy as np
-from numpy.typing import ArrayLike, NDArray
+from numpy.typing import ArrayLike, DTypeLike, NDArray
 
 
 class Generator:
     """Abstract base class of the value generators."""
 
+    DATA_DTYPES: List[DTypeLike] = []
+    """Accepted data types of the generator."""
+
     def __init__(self, data: NDArray) -> None:
-        self.dtype: np.dtype = data.dtype
+        self.dtype: DTypeLike = data.dtype
         self.na_rate: float = 0.0
 
     @classmethod
@@ -18,15 +25,40 @@ class Generator:
 
     @property
     def nan(self):
+        """The NA value of the generator, either NaN or NaT."""
         if np.issubdtype(self.dtype, np.datetime64):
             return np.datetime64("NaT")
         else:
             return np.nan
 
     def _make(self, size: int) -> NDArray:
+        """Hidden maker method. This method does the actual work before
+        :meth:`make()` continues with the postprocessing.
+
+        Args:
+            size (int): number of elements in returned array
+
+        Raises:
+            NotImplementedError: method must be overwritten.
+
+        Returns:
+            NDArray: valid array of given size
+        """
         raise NotImplementedError
 
     def make(self, size: int, with_na: bool = False) -> NDArray:
+        """Creates a new data array of a given size. The values are generatored
+        randomly for each execution. NA values can be inserted optionally.
+
+        Args:
+            size (int): number of elements in returned array
+            with_na (bool, optional): Allows to replicate NA occurrence in data.
+                If True, NA values are randomly inserted in the data. The rate
+                is fitted from the data. Defaults to False.
+
+        Returns:
+            NDArray: array with newly generatored values
+        """
         data = self._make(size=size).astype(self.dtype)
         if with_na:
             isna = np.random.uniform(size=size) < self.na_rate
