7247f59958a046d7db76dbf4b084a75c3c192ad5
kjanker <34544392+kjanker@users.noreply.github.com>
2022-01-15 10:10:43 +0100

    refactor regex generator

--- duper/generator/regex.py
+++ duper/generator/regex.py
@@ -1,13 +1,16 @@
 """
 A generator for string data that follow a fixed structure like IDs.
 """
+from __future__ import annotations
+
 import itertools
 import re
+from typing import List
 
 import numpy as np
 import pandas as pd
 import rstr
-from numpy.typing import NDArray
+from numpy.typing import DTypeLike, NDArray
 
 from .base import Generator
 
@@ -15,32 +18,78 @@ from .base import Generator
 class Regex(Generator):
     """Generator class recommended for strings with a repeating structure.
 
-    It creates values from a regular expression derived from the data.
+    It creates values from a regular expression and is best used for data that
+    follow a fixed structure like IDs.
 
     """
 
-    def __init__(self, data: NDArray[np.str_]) -> None:
-        super().__init__(data=data)
-        data_clean = data[~pd.isna(data)]
-        self.na_rate = 1 - len(data_clean) / len(data)
-        self.regex = self._beautify_regex(self._train_regex(data_clean))
+    DATA_DTYPES: List[DTypeLike] = [np.str_, np.object_]
+
+    def __init__(
+        self, regex: str, dtype: DTypeLike = None, na_rate: float = 0.0
+    ):
+        """Initializes a new generator based on the provided regular expression.
+        Consider using :meth:`from_data()` to fit the expression from data.
+
+        Args:
+            regex (str): a valid regular expression
+            dtype (DTypeLike, optional): by default, the dtype is derived from
+                **value**. Optionally, a valid numpy dtype can be provided.
+            na_rate (float, optional): rate at which NA occour in the data.
+                Must be in [0,1]. Defaults to 0.0.
+        """
+        self.regex = regex
+        self.dtype = dtype
+        self.na_rate = na_rate
 
     @classmethod
     def from_data(cls, data: NDArray):
-        return cls(data=data)
+        """Initializes a new generator from the provided **data**. This is
+        a convenience interface and the preferred method to create a new
+        generator instance.
+
+        The provided data is interpreted as string and used to train a regular
+        expression that can later be used to generate new data points.
+
+        Args:
+            data (NDArray): set of data, interpreted as strings
+        """
+        data = cls.validate(data=data)
+        vals = np.asarray(data[~pd.isna(data)]).astype(np.str_)
+        na_rate = 1 - vals.size / data.size
+        regex = cls._beautify_regex(cls._train_regex(vals))
+        return cls(regex=regex, dtype=data.dtype, na_rate=na_rate)
 
     def __str__(self) -> str:
         return f"{self.__class__.__name__} with '{self.regex}'>"
 
     def _make(self, size: int) -> NDArray:
+        """Hidden maker method. Creates an array of the given **size**
+        by building strings from the generator's regular expression.
+
+        Args:
+            size (int): number of elements in returned array.
+
+        Returns:
+            NDArray: array of strings.
+        """
         return np.array([rstr.xeger(self.regex) for _ in range(size)])
 
     @staticmethod
     def _train_regex(data: NDArray[np.str_]) -> str:
-        """Simple algorithm to derive a regular expression from a set of
-        strings. It loops the strings character by character, takes the n-th
+        """Simple algorithm to fit a regular expression on a set of strings.
+
+        It loops the strings character by character, takes the n-th
         characters of each string and builds a regular expression, allowing
         only those characters at this position.
+
+        Consider postprocessing the result with :meth:`_beautify_regex()`.
+
+        Args:
+            data (NDArray): array of strings, must not contain NA.
+
+        Returns:
+            str: raw regular expression, potentially bloated.
 
         """
         # break words in data into list of characters
@@ -90,6 +139,19 @@ class Regex(Generator):
 
     @staticmethod
     def _beautify_regex(regex: str) -> str:
+        """Makes regular expressions shorter and more elegant. This is used as
+        postprocessing after :meth:`_train_regex()`.
+
+        Args:
+            regex (str): a bloated regular expression
+
+        Returns:
+            str: a more compact regular expression
+
+        Examples:
+            >>> Regex._beautify_regex('[ABCD][123789][xyz]')
+            '[A-D][1-37-9][x-y]'
+        """
         for chars in [
             range(ord("0"), ord("9") - 1),
             range(ord("a"), ord("z") - 1),
